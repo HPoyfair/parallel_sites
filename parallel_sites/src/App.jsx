703a1a1764b292f04bf2e8db07a06b3b7cacae76
HPoyfair <haydenpoyfair@gmail.com>
2025-06-29 15:19:01 -0600

    react

---
+++ parallel_sites/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return(
+    <div className="container">
+      <header>
+        <h1>Parallel Sites</h1>
+        <p>Seth likes Goth mommies</p>
+      </header>
+      <main>
+        <section>
+          <h2>About</h2>
+          <p>butts nuts and cocanuts.</p>
+        </section>
+      </main>
+      <footer>
+        <p> please give us money, thank you</p>
+      </footer>
+    </div>
+  )
+
+}
+export default App
